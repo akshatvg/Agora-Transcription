e27a4b598b79e1f0798a58839265f23acd380640
Akshat Gupta <akshatvg23@gmail.com>
2021-03-28 23:24:35 +0530

    half working

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// Create Agora client and JavaScript Speech Recognition Init
+// Create Agora RTC client
 var client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+// JavaScript Speech Recognition Init
 var SpeechRecognition = window.webkitSpeechRecognition || window.speechRecognition;
 var recognition = new SpeechRecognition();
 var Content = "";
 recognition.continuous = true;
-
+// RTM Global Vars
+var isLoggedIn = false;
+// Local Tracks
 var localTracks = {
   videoTrack: null,
   audioTrack: null
@@ -15,23 +18,11 @@ var options = {
   appid: null,
   channel: null,
   uid: null,
-  token: null
+  token: null,
+  accountName: null
 };
 
-// The demo can auto join channel with params in url
-$(() => {
-  var urlParams = new URL(location.href).searchParams;
-  options.appid = urlParams.get("appid");
-  options.channel = urlParams.get("channel");
-  options.token = urlParams.get("token");
-  if (options.appid && options.channel) {
-    $("#appid").val(options.appid);
-    $("#token").val(options.token);
-    $("#channel").val(options.channel);
-    $("#join-form").submit();
-  }
-})
-
+// Join Channel
 $("#join-form").submit(async function (e) {
   e.preventDefault();
   $("#join").attr("disabled", true);
@@ -39,19 +30,23 @@ $("#join-form").submit(async function (e) {
     options.appid = $("#appid").val();
     options.token = $("#token").val();
     options.channel = $("#channel").val();
+    options.accountName = $('#accountName').val();
     await join();
   } catch (error) {
     console.error(error);
   } finally {
     $("#leave").attr("disabled", false);
     $("#transcribe").attr("disabled", false);
+    $("#note").attr("disabled", false);
   }
 })
 
+// Leave Channel
 $("#leave").click(function (e) {
   leave();
 })
 
+// Join Function
 async function join() {
   // Add event listener to play remote tracks when remote user publishes
   client.on("user-published", handleUserPublished);
@@ -64,14 +59,33 @@ async function join() {
     AgoraRTC.createMicrophoneAudioTrack(),
     AgoraRTC.createCameraVideoTrack()
   ]);
-  // play local video track
+  // Play local video track
   localTracks.videoTrack.play("local-player");
   $("#local-player-name").text(`localVideo(${options.uid})`);
-  // publish local tracks to channel
+  // Publish local tracks to channel
   await client.publish(Object.values(localTracks));
   console.log("Publish success");
+  // Create Agora RTM client
+  const clientRTM = AgoraRTM.createInstance($("#appid").val(), { enableLogUpload: false });
+  var accountName = $('#accountName').val();
+  // Login
+  clientRTM.login({ uid: accountName }).then(() => {
+    console.log('AgoraRTM client login success. Username: ' + accountName);
+    isLoggedIn = true;
+    // RTM Channel Join
+    var channelName = $('#channel').val();
+    channel = clientRTM.createChannel(channelName);
+    channel.join().then(() => {
+      console.log('AgoraRTM client channel join success.');
+    }).catch(error => {
+      console.log('AgoraRTM client channel join failed: ', error);
+    }).catch(err => {
+      console.log('AgoraRTM client login failure: ', err);
+    });
+  });
 }
 
+// Leave Function
 async function leave() {
   for (trackName in localTracks) {
     var track = localTracks[trackName];
@@ -91,11 +105,14 @@ async function leave() {
   $("#local-player-name").text("");
   $("#join").attr("disabled", false);
   $("#leave").attr("disabled", true);
+  $("#note").attr("disabled", true);
   $("#transcribe").attr("disabled", true);
   $("#stop-transcribe").attr("disabled", true);
+  $("#stop-note").attr("disabled", true);
   console.log("Client leaves channel success");
 }
 
+// Subscribe function
 async function subscribe(user, mediaType) {
   const uid = user.uid;
   // Subscribe to a remote user
@@ -116,38 +133,90 @@ async function subscribe(user, mediaType) {
   }
 }
 
+// User published callback
 function handleUserPublished(user, mediaType) {
   const id = user.uid;
   remoteUsers[id] = user;
   subscribe(user, mediaType);
 }
 
+// User unpublish callback
 function handleUserUnpublished(user) {
   const id = user.uid;
   delete remoteUsers[id];
   $(`#player-wrapper-${id}`).remove();
 }
 
-recognition.onresult = function (event) {
-  var current = event.resultIndex;
-  var transcript = event.results[current][0].transcript;
-  Content = Content + transcript + "<br>";
-  document.getElementById("actual-text").innerHTML = "UserID(" + options.uid + "): " + Content;
-};
-
-$("#transcribe").click(function () {
+// Start self notes
+$("#note").click(function () {
   console.log('Voice recognition is on.');
-  $("#stop-transcribe").attr("disabled", false);
+  $("#stop-note").attr("disabled", false);
+  $("#note").attr("disabled", true);
+  $("#stop-transcribe").attr("disabled", true);
+  $("#transcribe").attr("disabled", true);
   if (Content.length) {
     Content += ' ';
   }
   recognition.start();
 });
 
+// Stop self notes
+$("#stop-note").click(function () {
+  console.log('Voice recognition is off.');
+  recognition.stop();
+  recognition.onresult = function (event) {
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    Content = Content + transcript + "<br>";
+    document.getElementById("note-text").innerHTML = "<b><i>UserID(" + options.uid + "):</i></b> " + Content;
+  };
+  $("#note").attr("disabled", false);
+  $("#stop-note").attr("disabled", true);
+  $("#stop-transcribe").attr("disabled", true);
+  $("#transcribe").attr("disabled", false);
+});
+
+// Start transcription for all (RTM)
+$("#transcribe").click(function () {
+  console.log('Voice recognition is on.');
+  $("#transcribe").attr("disabled", true);
+  $("#stop-transcribe").attr("disabled", false);
+  $("#stop-note").attr("disabled", true);
+  $("#note").attr("disabled", true);
+  if (Content.length) {
+    Content += ' ';
+  }
+  recognition.start();
+});
+
+// Stop transcription for all (RTM)
 $("#stop-transcribe").click(function () {
   console.log('Voice recognition is off.');
   recognition.stop();
+  recognition.onresult = function (event) {
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    Content = Content + transcript + "<br>";
+    document.getElementById("actual-text").innerHTML = "<b><i>UserID(" + options.uid + "):</i></b> " + Content;
+    singleMessage = Content;
+    channel.sendMessage({ text: singleMessage }).then(() => {
+      console.log("Message sent successfully.");
+      console.log("Your message was: " + singleMessage + " by " + accountName);
+      $("#actual-text").append("<br> <b>Sender:</b> " + accountName + "<br> <b>Message:</b> " + singleMessage + "<br>");
+    }).catch(error => {
+      console.log("Message wasn't sent due to an error: ", error);
+    });
+    // Receive RTM Channel Message
+    channel.on('ChannelMessage', ({ text }, senderId) => {
+      console.log("Message received successfully.");
+      console.log("The message is: " + text + " by " + senderId);
+      $("#actual-text").append("<br> <b>Sender:</b> " + senderId + "<br> <b>Message:</b> " + text + "<br>");
+    });
+  };
+  $("#note").attr("disabled", false);
+  $("#stop-note").attr("disabled", true);
   $("#stop-transcribe").attr("disabled", true);
+  $("#transcribe").attr("disabled", false);
 });
 
 // Can't recognise voice
