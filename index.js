fe4dab39c9e8a1e152a74ae048268e4627d0ebb1
Akshat Gupta <akshatvg23@gmail.com>
2021-03-29 01:48:43 +0530

    transcription works with rtm!

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
 // JavaScript Speech Recognition Init
 var SpeechRecognition = window.webkitSpeechRecognition || window.speechRecognition;
 var recognition = new SpeechRecognition();
-var Content = "";
+var transContent = "";
+var noteContent = "";
 recognition.continuous = true;
 // RTM Global Vars
 var isLoggedIn = false;
@@ -77,6 +78,47 @@ async function join() {
     channel = clientRTM.createChannel(channelName);
     channel.join().then(() => {
       console.log('AgoraRTM client channel join success.');
+      // Start transcription for all (RTM)
+      $("#transcribe").click(function () {
+        console.log('Voice recognition is on.');
+        $("#transcribe").attr("disabled", true);
+        $("#stop-transcribe").attr("disabled", false);
+        $("#stop-note").attr("disabled", true);
+        $("#note").attr("disabled", true);
+        if (transContent.length) {
+          transContent += ' ';
+        }
+        recognition.start();
+      });
+      // Stop transcription for all (RTM)
+      $("#stop-transcribe").click(function () {
+        console.log('Voice recognition is off.');
+        recognition.stop();
+        recognition.onresult = function (event) {
+          var current = event.resultIndex;
+          var transcript = event.results[current][0].transcript;
+          transContent = transContent + transcript + "<br>";
+          singleMessage = transContent;
+          channel.sendMessage({ text: singleMessage }).then(() => {
+            console.log("Message sent successfully.");
+            console.log("Your message was: " + singleMessage + " by " + accountName);
+            $("#actual-text").append("<br> <b>Speaker:</b> " + accountName + "<br> <b>Message:</b> " + singleMessage + "<br>");
+            transContent = ''
+          }).catch(error => {
+            console.log("Message wasn't sent due to an error: ", error);
+          });
+        };
+        $("#note").attr("disabled", false);
+        $("#stop-note").attr("disabled", true);
+        $("#stop-transcribe").attr("disabled", true);
+        $("#transcribe").attr("disabled", false);
+      });
+      // Receive RTM Channel Message
+      channel.on('ChannelMessage', ({ text }, senderId) => {
+        console.log("Message received successfully.");
+        console.log("The message is: " + text + " by " + senderId);
+        $("#actual-text").append("<br> <b>Speaker:</b> " + senderId + "<br> <b>Message:</b> " + text + "<br>");
+      });
     }).catch(error => {
       console.log('AgoraRTM client channel join failed: ', error);
     }).catch(err => {
@@ -154,8 +196,8 @@ $("#note").click(function () {
   $("#note").attr("disabled", true);
   $("#stop-transcribe").attr("disabled", true);
   $("#transcribe").attr("disabled", true);
-  if (Content.length) {
-    Content += ' ';
+  if (noteContent.length) {
+    noteContent += ' ';
   }
   recognition.start();
 });
@@ -167,51 +209,9 @@ $("#stop-note").click(function () {
   recognition.onresult = function (event) {
     var current = event.resultIndex;
     var transcript = event.results[current][0].transcript;
-    Content = Content + transcript + "<br>";
-    document.getElementById("note-text").innerHTML = "<b><i>UserID(" + options.uid + "):</i></b> " + Content;
-  };
-  $("#note").attr("disabled", false);
-  $("#stop-note").attr("disabled", true);
-  $("#stop-transcribe").attr("disabled", true);
-  $("#transcribe").attr("disabled", false);
-});
-
-// Start transcription for all (RTM)
-$("#transcribe").click(function () {
-  console.log('Voice recognition is on.');
-  $("#transcribe").attr("disabled", true);
-  $("#stop-transcribe").attr("disabled", false);
-  $("#stop-note").attr("disabled", true);
-  $("#note").attr("disabled", true);
-  if (Content.length) {
-    Content += ' ';
-  }
-  recognition.start();
-});
-
-// Stop transcription for all (RTM)
-$("#stop-transcribe").click(function () {
-  console.log('Voice recognition is off.');
-  recognition.stop();
-  recognition.onresult = function (event) {
-    var current = event.resultIndex;
-    var transcript = event.results[current][0].transcript;
-    Content = Content + transcript + "<br>";
-    document.getElementById("actual-text").innerHTML = "<b><i>UserID(" + options.uid + "):</i></b> " + Content;
-    singleMessage = Content;
-    channel.sendMessage({ text: singleMessage }).then(() => {
-      console.log("Message sent successfully.");
-      console.log("Your message was: " + singleMessage + " by " + accountName);
-      $("#actual-text").append("<br> <b>Sender:</b> " + accountName + "<br> <b>Message:</b> " + singleMessage + "<br>");
-    }).catch(error => {
-      console.log("Message wasn't sent due to an error: ", error);
-    });
-    // Receive RTM Channel Message
-    channel.on('ChannelMessage', ({ text }, senderId) => {
-      console.log("Message received successfully.");
-      console.log("The message is: " + text + " by " + senderId);
-      $("#actual-text").append("<br> <b>Sender:</b> " + senderId + "<br> <b>Message:</b> " + text + "<br>");
-    });
+    noteContent = noteContent + transcript + "<br>";
+    $("#note-text").append("<b><i>UserID(" + options.uid + "):</i></b> " + noteContent);
+    noteContent = '';
   };
   $("#note").attr("disabled", false);
   $("#stop-note").attr("disabled", true);
